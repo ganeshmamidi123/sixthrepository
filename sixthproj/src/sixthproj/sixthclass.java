b61eb03bd3de10a76e64b52eadf7d486bcfda400
ganeshmamidi123 <ganesh.m3456@gmail.com>
2021-02-02 16:34:13 +0530

    sss

--- sixthproj/src/sixthproj/sixthclass.java
+++ sixthproj/src/sixthproj/sixthclass.java
@@ -2,7 +2,7 @@ package sixthproj;
 
 public class sixthclass {
 	public static void main(String args []) {
-		System.out.println("this is the sixth class1");
+		System.out.println("this is the sixth class2..");
 	}
 
 }
